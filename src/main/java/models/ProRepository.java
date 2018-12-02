6d2ade6bf102249a65b44190de07d3bae14688da
Alex Widner <manny1806@gmail.com>
2018-12-01 18:56:47 -0800

    added cors

--- src/main/java/models/ProRepository.java
+++ src/main/java/models/ProRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RepositoryRestResource(collectionResourceRel = "pro", path = "pro")
 public interface ProRepository extends MongoRepository<Pro, String> {
 
